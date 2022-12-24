b993495b3c0e6a8d6bd2acc808f69fa4533907f7
Abdul Basit <81741435+BasitShafiq@users.noreply.github.com>
2022-12-24 21:26:09 +0500

    Create IsSameBinaryTree.cpp

---
+++ IsSameBinaryTree.cpp
@@ -0,0 +1,14 @@
+bool isSameTree(TreeNode* p, TreeNode* q) {
+	if (p == NULL && q == NULL)
+	{
+		return true;
+	}
+	if (p != NULL && q != NULL)
+	{
+		if (p->val == q->val&& isSameTree(p->left, q->left)&& isSameTree(p->right, q->right))
+		{
+			return true;
+		}
+	}
+	return false;
+}
