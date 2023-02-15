6d53ce9d60e04b5d8bccbf0e231b3bc09073da79
Abdul Basit <81741435+BasitShafiq@users.noreply.github.com>
2023-02-15 21:42:52 +0500

    Update maxSubArray.cpp

--- maxSubArray.cpp
+++ maxSubArray.cpp
@@ -16,3 +16,25 @@ public:
 	return largest;
     }
 };
+
+
+// Linear Time
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+ 	int largest = INT_MIN;
+	int temp = 0;
+	
+	for (int i = 0; i < nums.size(); i++)
+	{
+		
+		if (temp < 0)
+		{
+			temp = 0;
+		}
+		temp = temp + nums[i];
+		largest = max(largest, temp);
+	}
+	return largest;
+    }
+};
